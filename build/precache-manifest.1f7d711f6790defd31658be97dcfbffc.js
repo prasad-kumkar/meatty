ae8c32bdf5ccc6fe73df49b00d422460ea89e06f
prasad-kumkar <pk111_@live.com>
2020-10-29 04:11:10 +0530

    deploy settings

---
+++ build/precache-manifest.1f7d711f6790defd31658be97dcfbffc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e41fda431c9a7378e1c0895af740042",
+    "url": "/meatty/index.html"
+  },
+  {
+    "revision": "86220bcdd8061fa366d9",
+    "url": "/meatty/static/css/main.f21f04ed.chunk.css"
+  },
+  {
+    "revision": "2017dab507791945932a",
+    "url": "/meatty/static/js/2.64a48036.chunk.js"
+  },
+  {
+    "revision": "0dc713a05373e5c9a88cb999f31ae6e4",
+    "url": "/meatty/static/js/2.64a48036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86220bcdd8061fa366d9",
+    "url": "/meatty/static/js/main.27aa9cf5.chunk.js"
+  },
+  {
+    "revision": "ae653c4f18e58960dce7",
+    "url": "/meatty/static/js/runtime-main.5e8a1cfc.js"
+  },
+  {
+    "revision": "edf1f3e3f90bddc71c66c30a3d119c51",
+    "url": "/meatty/static/media/banner.edf1f3e3.jpg"
+  },
+  {
+    "revision": "ecbbf92cb9f31d8f274a606f007bece4",
+    "url": "/meatty/static/media/beef.ecbbf92c.jpg"
+  },
+  {
+    "revision": "15ae3f4bbb44a57dc92f12c31e8a2300",
+    "url": "/meatty/static/media/checkout-ad.15ae3f4b.jpg"
+  },
+  {
+    "revision": "6f7e08f726fab3d942ae42462183a5e7",
+    "url": "/meatty/static/media/logo.6f7e08f7.png"
+  },
+  {
+    "revision": "bef8b827f596da829fcb548bd75cb42b",
+    "url": "/meatty/static/media/pork.bef8b827.jpg"
+  }
+]);
